e9385af039162fb470ef32a58e0411dc90baae76
José Leonardo Cordeiro Bahia <leonardo_cordeirobahia@hotmail.com>
2020-09-22 23:27:46 -0300

    Criação dos repository
    Foram criado os repository de Jogadora e Pontuacao para realizarmos transações no banco de dados.

---
+++ workspace/Desafio_PublicaTecnologia/src/main/java/com/joseleonardo/model/repository/JogadoraRepository.java
@@ -0,0 +1,9 @@
+package com.joseleonardo.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.joseleonardo.model.Jogadora;
+
+public interface JogadoraRepository extends JpaRepository<Jogadora, Long>{
+
+}
---
+++ workspace/Desafio_PublicaTecnologia/src/main/java/com/joseleonardo/model/repository/PontuacaoRepository.java
@@ -0,0 +1,9 @@
+package com.joseleonardo.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.joseleonardo.model.Pontuacao;
+
+public interface PontuacaoRepository extends JpaRepository<Pontuacao, Long>{
+
+}
