79ecfc2527b507a550d959b31a9c616889e1723f
José Leonardo Cordeiro Bahia <leonardo_cordeirobahia@hotmail.com>
2020-09-22 22:22:18 -0300

    Criação do projeto
    Criei o projeto com Spring com algumas dependências iniciais.

---
+++ workspace/Desafio_PublicaTecnologia/src/main/java/com/joseleonardo/DesafioPublicaTecnologiaApplication.java
@@ -0,0 +1,13 @@
+package com.joseleonardo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPublicaTecnologiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPublicaTecnologiaApplication.class, args);
+	}
+
+}
