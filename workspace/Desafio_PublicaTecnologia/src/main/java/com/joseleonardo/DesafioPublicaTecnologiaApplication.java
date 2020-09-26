fa68aaedc6a277885fdf2b7a7c2bb535941abb29
José Leonardo Cordeiro Bahia <leonardo_cordeirobahia@hotmail.com>
2020-09-26 17:30:59 -0300

    Validação e autenticação de login

--- workspace/Desafio_PublicaTecnologia/src/main/java/com/joseleonardo/DesafioPublicaTecnologiaApplication.java
+++ workspace/Desafio_PublicaTecnologia/src/main/java/com/joseleonardo/DesafioPublicaTecnologiaApplication.java
@@ -7,16 +7,25 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.joseleonardo.model.Jogadora;
 import com.joseleonardo.model.repository.JogadoraRepository;
 
 @SpringBootApplication
-public class DesafioPublicaTecnologiaApplication implements CommandLineRunner{
+public class DesafioPublicaTecnologiaApplication implements WebMvcConfigurer, CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesafioPublicaTecnologiaApplication.class, args);
 	}
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/login").setViewName("/login");
+		
+	}
+	
 	
 	@Autowired
 	private JogadoraRepository jogadoraRepository;
@@ -24,11 +33,12 @@ public class DesafioPublicaTecnologiaApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(); 
+
 		String resultado = encoder.encode("123");
 		System.out.println(resultado);
 		
-		Jogadora jogadora = new Jogadora("Maria", "maria@gmail.com", "123");
+		Jogadora jogadora = new Jogadora("Maria", "maria@gmail.com", resultado);
 		jogadoraRepository.saveAll(Arrays.asList(jogadora));
 		
 	}
--- workspace/Desafio_PublicaTecnologia/src/main/java/com/joseleonardo/security/WebConfigSecurity.java
+++ workspace/Desafio_PublicaTecnologia/src/main/java/com/joseleonardo/security/WebConfigSecurity.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,24 +15,32 @@ import com.joseleonardo.service.JogadoraService;
 @Configuration
 @EnableWebSecurity
 public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
 	private JogadoraService jogadoraService;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.GET, "/").permitAll().anyRequest()
-				.authenticated().and().formLogin().permitAll().and().logout()
-				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+		http.csrf()
+		.disable()
+		.authorizeRequests() 
+		.antMatchers(HttpMethod.GET, "/login").permitAll() 
+		.anyRequest().authenticated()
+		.and()
+		.formLogin().permitAll()
+        .loginPage("/login")
+        .defaultSuccessUrl("/cadastro/pontuacao")
+        .failureUrl("/login?error=true")
+        .and()
+        .logout().logoutSuccessUrl("/login")
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
 	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(jogadoraService).passwordEncoder(new BCryptPasswordEncoder());
 	}
+	
 
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/static/**");
-	}
+
 }
