ad9219be422ce683bfbee8e06c366053e2d20fc0
José Leonardo Cordeiro Bahia <leonardo_cordeirobahia@hotmail.com>
2020-09-28 23:16:39 -0300

    Documentação dos repository

--- workspace/Desafio_PublicaTecnologia/src/main/java/com/joseleonardo/model/repository/JogadoraRepository.java
+++ workspace/Desafio_PublicaTecnologia/src/main/java/com/joseleonardo/model/repository/JogadoraRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.Query;
 
 import com.joseleonardo.model.Jogadora;
 
+/* Criação do repository de Jogadora para realizar transições no banco de dados  */
 public interface JogadoraRepository extends JpaRepository<Jogadora, Long>{
 
+	/* Pega na requisção o e-mail e ve se existe no banco de dados */
 	@Query("SELECT j FROM Jogadora j WHERE j.email = ?1")
 	Jogadora findJogadoraByEmail(String email);
 	
--- workspace/Desafio_PublicaTecnologia/src/main/java/com/joseleonardo/model/repository/PontuacaoRepository.java
+++ workspace/Desafio_PublicaTecnologia/src/main/java/com/joseleonardo/model/repository/PontuacaoRepository.java
@@ -5,17 +5,22 @@ import org.springframework.data.jpa.repository.Query;
 
 import com.joseleonardo.model.Pontuacao;
 
+/* Criação do repository de Pontuacao para realizar transições no banco de dados  */
 public interface PontuacaoRepository extends JpaRepository<Pontuacao, Long>{
 	
+	/* Retorna o valor mínimo do mínimo da temporada */
 	@Query("SELECT MIN(p.minimoTemporada) FROM Pontuacao p")
 	int findMinTemporada();
 	
+	/* Retorna o valor máximo do máximo da temporada */
 	@Query("SELECT MAX(p.maximoTemporada) FROM Pontuacao p")
 	int findMaxTemporada();
 	
+	/* Retorna o valor máximo do recorde mínimo da temporada */
 	@Query("SELECT MAX(p.quebraRecordeMin) FROM Pontuacao p")
 	int findRecordeMin();
 	
+	/* Retorna o valor máximo do recorde máximo da temporada */
 	@Query("SELECT MAX(p.quebraRecordeMax) FROM Pontuacao p")
 	int findRecordeMax();
 }
