98cc3f973eedb4208775ce34a16cdbd23bfad4cf
José Leonardo Cordeiro Bahia <leonardo_cordeirobahia@hotmail.com>
2020-09-28 23:35:08 -0300

    Documentação do WebSecurity

--- workspace/Desafio_PublicaTecnologia/src/main/java/com/joseleonardo/security/WebConfigSecurity.java
+++ workspace/Desafio_PublicaTecnologia/src/main/java/com/joseleonardo/security/WebConfigSecurity.java
@@ -19,23 +19,25 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private JogadoraService jogadoraService;
 
+	/* Configura as requisições de acesso por HTTP */
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf()
-		.disable()
-		.authorizeRequests() 
-		.antMatchers(HttpMethod.GET, "/login").permitAll() 
+		.disable()/* Desativa as configurações padrão de memória */
+		.authorizeRequests() /* Restringir acessos  */
+		.antMatchers(HttpMethod.GET, "/login").permitAll()/* Qualquer usário acessa a página de login */
 		.anyRequest().authenticated()
 		.and()
 		.formLogin().permitAll()
-        .loginPage("/login")
-        .defaultSuccessUrl("/menu")
-        .failureUrl("/login?error=true")
+        .loginPage("/login")/* Redireciona para a página de login com bootstrap */
+        .defaultSuccessUrl("/menu")/* Após logar, redirecionar para a página de menu */
+        .failureUrl("/login?error=true")/* Se o login estiver errado, redirecionar para o formulário de login */
         .and()
-        .logout().logoutSuccessUrl("/login")
-		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+        .logout().logoutSuccessUrl("/login")/* Quando realizar o logout, redirecionar para o formulário de login */
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));/* Mapea a URL de logout e inválida o usuário autenticado */
 	}
 
+	/* Cria autenticação do usuário */
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(jogadoraService).passwordEncoder(new BCryptPasswordEncoder());
